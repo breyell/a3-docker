a2efe5fbdc8fa6055a02066a58636b2bacd5d6be
Brendon Reyell <brendon@apostrophecms.com>
2021-01-14 17:48:26 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// So the CSS is part of the bundle
+import './index.scss';
+
+// Your own project level JS may go here
+console.log('Hello World');
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'modules/asset/ui/public'),
+    filename: 'site.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
